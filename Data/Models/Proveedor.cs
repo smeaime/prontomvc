1e361a03ab3c0ba7588074bb72873323975829e1
ws03\Administrador <edesantis@live.com.ar>
2015-01-05 15:44:53 -0300

    no message
    Former-commit-id: 138483fc28b8c89ce33d2366a0c7a3949e62e6db

--- Data/Models/Proveedor.cs
+++ Data/Models/Proveedor.cs
@@ -145,6 +145,7 @@ namespace ProntoMVC.Data.Models
         public Nullable<int> IdCuentaAplicacion { get; set; }
         public string CodigoCategoriaIIBBAlternativo { get; set; }
         public Nullable<System.DateTime> FechaInicialControlComprobantes { get; set; }
+        public string ResolucionAfip3668 { get; set; }
     
         public virtual ICollection<DetalleProveedor> DetalleProveedores { get; set; }
         public virtual ICollection<DetalleProveedoresIB> DetalleProveedoresIBs { get; set; }
