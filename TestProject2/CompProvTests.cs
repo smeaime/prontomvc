9b555e8405a9ac5dca7a7fedf7e13e9b99579a87
mscalella <mscalella911@gmail.com>
2015-01-07 07:26:14 -0300

    mensaje cualquiera
    Former-commit-id: e55c4b38937cac908e828510010d0b6291dee759

--- TestProject2/CompProvTests.cs
+++ TestProject2/CompProvTests.cs
@@ -29,7 +29,7 @@ namespace ProntoMVC.Tests
 {
     using System.Web.Mvc;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    
+
 
 
 
@@ -46,12 +46,18 @@ namespace ProntoMVC.Tests
 
 
 
-        
+
 
         [TestMethod()]
         public void UsandoItemdeprespuestodeObra()
         {
+            // hacer
+        }
 
+        [TestMethod()]
+        public void Un_Admin_Comun_intenta_cambiar_su_nivel_de_RMs_pero_no_tiene_acceso_al_modulo()
+        {
+            // hacer
         }
 
 
@@ -68,24 +74,34 @@ namespace ProntoMVC.Tests
             //If the mock behaviour is complex, you end up in testing your mock instead of your business code.
 
 
-            Repo.UnitOfWork unitOfWork = new Repo.UnitOfWork(sc);
-            Servicio.ComprobanteProveedorService comprobanteproveedorService = new Servicio.ComprobanteProveedorService(unitOfWork);
-            ComprobanteProveedor o = new ComprobanteProveedor();
-            int? numerooriginal;
 
-            o = PreparoObjeto();
-            //o = fondoFijoService.ObtenerPorId(10);
-            numerooriginal = o.NumeroComprobante2;
-            o.NumeroComprobante2 += 14; // para que de distinto
-            o.IdComprobanteProveedor = 0;
+            using (TransactionScope _scope = new TransactionScope())
+            {
 
-            comprobanteproveedorService.Guardar(o);
-            unitOfWork.Save();
 
-            ComprobanteProveedor d = comprobanteproveedorService.ObtenerPorId(10);
 
-            Assert.AreNotEqual(numerooriginal, d.NumeroComprobante2);
-            //Assert.AreEqual(o, d);
+                Repo.UnitOfWork unitOfWork = new Repo.UnitOfWork(sc);
+                Servicio.ComprobanteProveedorService comprobanteproveedorService = new Servicio.ComprobanteProveedorService(unitOfWork);
+                ComprobanteProveedor o = new ComprobanteProveedor();
+                int? numerooriginal;
+
+                o = PreparoObjeto();
+                //o = fondoFijoService.ObtenerPorId(10);
+                numerooriginal = o.NumeroComprobante2;
+                o.NumeroComprobante2 += 14; // para que de distinto
+                o.IdComprobanteProveedor = 0;
+
+                comprobanteproveedorService.Guardar(o);
+                unitOfWork.Save();
+
+                ComprobanteProveedor d = comprobanteproveedorService.ObtenerPorId(10);
+
+                Assert.AreNotEqual(numerooriginal, d.NumeroComprobante2);
+                //Assert.AreEqual(o, d);
+
+
+                // _scope.Complete();
+            }
         }
 
 
@@ -189,5 +205,212 @@ namespace ProntoMVC.Tests
 
         }
 
+
+
+
+
+
+
+
+
+
+        [TestMethod()]
+        public void BatchUpdateComprobanteProvTest()
+        {
+            //http://msdn.microsoft.com/en-us/library/gg416511(v=vs.98).aspx
+
+
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            ComprobanteProveedor c = PreparoObjeto();
+
+            ViewModels.ViewModelComprobanteProveedor o = new ViewModels.ViewModelComprobanteProveedor();
+            AutoMapper.Mapper.CreateMap<ComprobanteProveedor, ViewModels.ViewModelComprobanteProveedor>();
+            AutoMapper.Mapper.Map(c, o);
+
+
+
+
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            JsonResult expected = null;            // TODO: Initialize to an appropriate value
+            expected = new JsonResult();
+
+            //FacturaController.JsonResponse res = new FacturaController.JsonResponse();
+
+            // res.Status = Status.Error;
+            // res.Errors = GetModelStateErrorsAsString(this.ModelState);
+            //res.Message = "Error al obtener CAE : El nÃºmero o fecha del comprobante no se corresponde con el prÃ³ximo a autorizar. Consultar metodo FECompUltimoAutorizado. - Ultimo numero 974";
+            //expected = Controller.Json(res);
+            JsonResult actual;
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+            // -no entiendo: cómo es que MOCKEAS el controller, si el controller es el SUT! ( es decir, estás fingiendo lo mismo 
+            // que estas probando!!) 
+            // -Es que no mockeas el controller: mockeas su ControllerContext +  HttpContextBase + IPrincipal
+            // ComprobanteProveedorController target = new ComprobanteProveedorController(); // TODO: Initialize to an appropriate value
+            ComprobanteProveedorController target = GetMockedComprobanteProveedorController();
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+
+
+
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+
+
+
+            //si llamás prepo a BatchFactura, el initialize no se corrió, el controlador no tiene el contexto inicializado (db es null)
+            // http://stackoverflow.com/questions/5507505/why-does-my-asp-net-mvc-controller-not-call-its-base-class-initialize-method-d
+            //target.db = db;
+            actual = target.BatchUpdate(o);
+
+
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+            //  http://www.seankenny.me/blog/2013/05/02/unit-testing-asp-dot-net-mvc-jsonresults/
+            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            var obj = serializer.Serialize(actual.Data) as dynamic;
+            var customer = serializer.Deserialize<dynamic>(obj);
+
+            //Assert.AreEqual(1, customer["Id"]);
+            //Assert.AreEqual("John", customer["Name"]);
+
+            Console.Write(customer["Errors"]);
+            //Assert.Inconclusive(customer["Errors"].ToString());
+            //Console.Write(customer["Warnings"]);
+            //Assert.AreEqual("John", customer["Success"].ToString());
+            //Assert.AreEqual("John", customer["Errors"].ToString());
+            //Assert.AreEqual(4544, customer["IdComprobanteProveedor"].ToString());
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+            // Assert.AreEqual(expected, actual);
+            // Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+
+
+
+
+        static private ComprobanteProveedorController GetMockedComprobanteProveedorController()
+        {
+
+            // -no entiendo: cómo es que MOCKEAS el controller, si el controller es el SUT! ( es decir, estás fingiendo lo mismo 
+            // que estas probando!!) 
+            // -Es que no mockeas el controller: mockeas su ControllerContext +  HttpContextBase + IPrincipal
+
+
+            var c = new ComprobanteProveedorController();
+
+
+            //var httpContextMock = new Mock<HttpContextBase>();
+            //var serverMock = new Mock<HttpServerUtilityBase>();
+            //serverMock.Setup(x => x.MapPath("~/App_Data")).Returns(@"C:\Backup\BDL\ProntoMVC\ProntoMVC\App_Data");  // es case sensitive!!!!  no pongas "app_data"    // (@"C:\Users\Mariano\Desktop");
+            //httpContextMock.Setup(x => x.Server).Returns(serverMock.Object);
+            //var sut = new ComprobanteProveedorController();
+            //sut.ControllerContext = new ControllerContext(httpContextMock.Object, new System.Web.Routing.RouteData(), sut);
+
+
+            // http://stackoverflow.com/questions/1389744/testing-controller-action-that-uses-user-identity-name
+            // por qué necesito usar un mock para hacer que uso un usuario específico? No puedo usar el controller derecho viejo sin mockear?
+            // -sea lo que fuere, vos necesitás inyectar 
+
+            var identity = new GenericIdentity("superadmin");
+            var userMock = new Mock<IPrincipal>();
+            userMock.Setup(p => p.IsInRole("Administrador")).Returns(true);
+            userMock.SetupGet(x => x.Identity.Name).Returns("superadmin");
+
+
+            // mockeas ControllerContext +  HttpContextBase + IPrincipal
+
+            var contextMock = new Mock<HttpContextBase>();
+            contextMock.SetupGet(ctx => ctx.User)
+                       .Returns(userMock.Object);
+
+
+            var controllerContext = new Mock<ControllerContext>();
+
+            controllerContext.SetupGet(con => con.HttpContext)
+                                 .Returns(contextMock.Object);
+
+
+
+
+
+
+            //controllerContext.SetupGet(p => p.HttpContext.Session["BasePronto"]).Returns(Generales.BaseDefault((Guid)Membership.GetUser().ProviderUserKey));
+            controllerContext.SetupGet(p => p.HttpContext.Session["BasePronto"]).Returns("DemoProntoWeb");
+            //  controllerContext.SetupGet(p => p.HttpContext.User.Identity.Name).Returns(_testEmail);
+            controllerContext.SetupGet(p => p.HttpContext.Request.IsAuthenticated).Returns(true);
+            controllerContext.SetupGet(p => p.HttpContext.Response.Cookies).Returns(new HttpCookieCollection());
+
+            controllerContext.Setup(p => p.HttpContext.Request.Form.Get("ReturnUrl")).Returns("sample-return-url");
+            controllerContext.Setup(p => p.HttpContext.Request.Params.Get("q")).Returns("sample-search-term");
+
+            c.ControllerContext = controllerContext.Object;
+
+
+
+
+
+
+
+
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+            // necesito llamar a mano al Initialize  http://stackoverflow.com/questions/1452665/how-to-trigger-initialize-method-while-trying-to-unit-test
+            //  http://stackoverflow.com/questions/1452418/how-do-i-mock-the-httpcontext-in-asp-net-mvc-using-moq
+
+
+            //var requestContext = new System.Web.Routing.RequestContext(controllerContext.Object, new System.Web.Routing.RouteData());
+            //var requestContext = new System.Web.Routing.RequestContext(contextMock.Object, new System.Web.Routing.RouteData());
+            //IController controller = c;
+            //controller.Execute(requestContext);
+
+
+            c.FakeInitialize(sc);
+
+            // este tipo sugiere directamente sacar del Initialize el codigo y meterlo en un metodo para llamarlo aparte
+            // http://stackoverflow.com/questions/5769163/asp-net-mvc-unit-testing-override-initialize-method
+            // I suggest you to factor out your custom Initialize() logic out into different method. Then create fake (stub) subclass with 
+            // public method that calls this factored out protected Initialzie. Are you with me?
+
+            /////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////
+
+
+
+
+            return c;
+        }
     }
 }
