48c3611ae7ec46187c0c4d0b04c0a3e22974c874
mscalella <mscalella911@gmail.com>
2015-01-28 14:56:00 -0300

    mensaje cualquiera
    Former-commit-id: 5124b66f02584cb2372ec032a903dece257c4a74 [formerly 7f27eebf6ae38ace50b9eb93e260f88295f073b5]
    Former-commit-id: 583ffd6b3908983080a479699242d1b4310fb820

--- ProntoMVC/Global.asax.cs
+++ ProntoMVC/Global.asax.cs
@@ -10,6 +10,9 @@ using System.Web.Security;
 
 using System.Configuration;
 
+using StackExchange.Profiling;
+using StackExchange.Profiling.EntityFramework6;
+
 
 namespace ProntoMVC
 {
@@ -49,8 +52,27 @@ namespace ProntoMVC
             );
         }
 
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal)
+            {
+                MiniProfiler.Start();
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
+        }
+
+
         protected void Application_Start()
         {
+
+
+            MiniProfilerEF6.Initialize();
+
 
             ViewEngines.Engines.Add(new RazorViewEngine());
 
@@ -214,7 +236,7 @@ namespace ProntoMVC
                 nombreLargo = AppDomain.CurrentDomain.BaseDirectory + @"Error\" + DateTime.Today.ToString("dd-MM-yy") + ".txt";
             }
 
-            string log="";
+            string log = "";
             try
             {
                 log = System.IO.File.ReadAllText(nombreLargo);
@@ -222,7 +244,7 @@ namespace ProntoMVC
             }
             catch (Exception)
             {
-                
+
                 //throw;
             }
 
@@ -258,19 +280,20 @@ namespace ProntoMVC
 
 
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 try
                 {
                     ErrHandler.WriteError(ex);
                 }
                 catch (Exception)
                 {
-                    
+
                     //throw;
                 }
-                        
-                
-            
+
+
+
             };
 
 
