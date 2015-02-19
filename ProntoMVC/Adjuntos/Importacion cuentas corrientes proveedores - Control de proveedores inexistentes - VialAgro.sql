7b8f979e1f149d78698815da6b45f0e64da475ec
mscalella <mscalella911@gmail.com>
2015-02-19 05:48:33 -0300

    mensaje cualquiera
    Former-commit-id: 1ff2978105b8831dae2ba236ce54a484e6a53ac4

--- ProntoMVC/Adjuntos/Importacion cuentas corrientes proveedores - Control de proveedores inexistentes - VialAgro.sql
+++
@@ -1,4 +0,0 @@
-select * 
-from _TempMayoresProveedores
-where not exists(Select Top 1 p.idproveedor from proveedores p where p.cuit=_TempMayoresProveedores.Cuit COLLATE SQL_Latin1_General_CP1_CI_AS)
-order by tercero
