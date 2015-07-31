9dcf7e313fea8cdc454ffdc14205b830af5c49d7
Mariano <mscalella911@gmail.com>
2015-07-31 16:45:35 -0300

    'sdfsadf'
    Former-commit-id: e7fba0ce27dea1f2284956dcd6b3d686a6aac5ae

--- ProntoMVC/Controllers/NotaDebitoController.cs
+++ ProntoMVC/Controllers/NotaDebitoController.cs
@@ -262,13 +262,13 @@ namespace ProntoMVC.Controllers
             int pageSize = rows ;
             int currentPage = page ;
 
-            var data = (from a in db.NotasDebitoes
-                        from b in db.DescripcionIvas.Where(v => v.IdCodigoIva == a.IdCodigoIva).DefaultIfEmpty()
-                        from c in db.Obras.Where(v => v.IdObra == a.IdObra).DefaultIfEmpty()
-                        from d in db.Vendedores.Where(v => v.IdVendedor == a.IdVendedor).DefaultIfEmpty()
-                        from e in db.Empleados.Where(v => v.IdEmpleado == a.IdUsuarioIngreso).DefaultIfEmpty()
-                        from f in db.Empleados.Where(y => y.IdEmpleado == a.IdUsuarioAnulacion).DefaultIfEmpty()
-                        from g in db.Provincias.Where(v => v.IdProvincia == a.IdProvinciaDestino).DefaultIfEmpty()
+            var data = (from a in pagedQuery
+                        //from b in db.DescripcionIvas.Where(v => v.IdCodigoIva == a.IdCodigoIva).DefaultIfEmpty()
+                        //from c in db.Obras.Where(v => v.IdObra == a.IdObra).DefaultIfEmpty()
+                        //from d in db.Vendedores.Where(v => v.IdVendedor == a.IdVendedor).DefaultIfEmpty()
+                        //from e in db.Empleados.Where(v => v.IdEmpleado == a.IdUsuarioIngreso).DefaultIfEmpty()
+                        //from f in db.Empleados.Where(y => y.IdEmpleado == a.IdUsuarioAnulacion).DefaultIfEmpty()
+                        //from g in db.Provincias.Where(v => v.IdProvincia == a.IdProvinciaDestino).DefaultIfEmpty()
                         select new
                         {
                             a.IdNotaDebito,
@@ -280,7 +280,7 @@ namespace ProntoMVC.Controllers
                             a.Anulada,
                             ClienteCodigo = a.Cliente.CodigoCliente,
                             ClienteNombre = a.Cliente.RazonSocial,
-                            DescripcionIva = b != null ? b.Descripcion : "",
+                            DescripcionIva = a.DescripcionIva != null ? a.DescripcionIva.Descripcion : "",
                             ClienteCuit = a.Cliente.Cuit,
                             TotalGravado = (a.ImporteTotal ?? 0) - (a.ImporteIva1 ?? 0) - (a.PercepcionIVA ?? 0) - (a.RetencionIBrutos1 ?? 0) - (a.RetencionIBrutos2 ?? 0) - (a.RetencionIBrutos3 ?? 0) - (a.OtrasPercepciones1 ?? 0) - (a.OtrasPercepciones2 ?? 0) - (a.OtrasPercepciones3 ?? 0),
                             TotalIva = a.ImporteIva1,
@@ -289,14 +289,14 @@ namespace ProntoMVC.Controllers
                             TotalOtrasPercepciones = (a.OtrasPercepciones1 ?? 0) + (a.OtrasPercepciones2 ?? 0) + (a.OtrasPercepciones3 ?? 0),
                             a.ImporteTotal,
                             MonedaAbreviatura = a.Moneda.Abreviatura,
-                            Obra = c != null ? c.NumeroObra : "",
-                            Vendedor = d != null ? d.Nombre : "",
-                            ProvinciaDestino = g != null ? g.Nombre : "",
+                            Obra = a.Obra != null ? a.Obra.NumeroObra : "",
+                            Vendedor = a.Vendedore != null ? a.Vendedore.Nombre : "",
+                            ProvinciaDestino = a.Provincia != null ? a.Provincia.Nombre : "",
                             a.FechaAnulacion,
-                            UsuarioAnulo = f != null ? f.Nombre : "",
+                            UsuarioAnulo = a.Empleado != null ? a.Empleado.Nombre : "",
                             a.NumeroCuota,
                             a.FechaIngreso,
-                            UsuarioIngreso = e != null ? e.Nombre : "",
+                            UsuarioIngreso = a.Empleado1 != null ? a.Empleado1.Nombre : "",
                             a.CAE,
                             a.RechazoCAE,
                             a.FechaVencimientoORechazoCAE,
