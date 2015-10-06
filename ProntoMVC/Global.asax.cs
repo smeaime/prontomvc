6c9ebcfc4a8e558079da9314d04c53bef2094452
Mariano <mscalella911@gmail.com>
2015-10-06 09:19:09 -0300

    'sdfsadf'
    Former-commit-id: 1ba85a7bd3a3a2c18badd5d474d0a0ba89934103 [formerly 22aee51be1445c301c152f9bd657e63467a71c49]
    Former-commit-id: 781bf3a47bc67e568b4b2fd368ff8b4e5ad1a6df

--- ProntoMVC/Global.asax.cs
+++ ProntoMVC/Global.asax.cs
@@ -155,6 +155,15 @@ namespace ProntoMVC
                 Exception lastErrorWrapper = Server.GetLastError();
 
 
+                if (lastErrorWrapper==null)
+                {
+                    // http://stackoverflow.com/questions/343014/asp-net-custom-error-page-server-getlasterror-is-null
+
+                    // Try using something like Server.Transfer("~/ErrorPage.aspx"); from within the Application_Error() method of global.asax.cs
+                    Server.Transfer("~/Shared/SinConexion.cshtml");
+                }
+
+
                 Exception lastError = lastErrorWrapper;
                 if (lastErrorWrapper.InnerException != null) lastError = lastErrorWrapper.InnerException;
 
@@ -396,8 +405,15 @@ namespace ProntoMVC
                     // por qué la llamada al ErrorController necesita del Membership (y por lo tanto, de la conexion SQL)?
                     //http://stackoverflow.com/questions/1171035/asp-net-mvc-custom-error-handling-application-error-global-asax
                     //ssss
-                    Response.Redirect("~/Views/Shared/SinConexion.cshtml");
-                    throw;
+                    
+                    // http://stackoverflow.com/questions/15894254/how-to-solve-redirect-loop
+
+                    routeData.Values["action"] = "SinConexion";
+                    IController errorsController2 = new Controllers.ErrorController();
+                    var rc2 = new RequestContext(new HttpContextWrapper(Context), routeData);
+                    errorsController2.Execute(rc);
+                    // Response.Redirect("~/Views/Shared/SinConexion.cshtml");
+                    //throw;
                 }
                 catch (Exception)
                 {
