32b883d8fea66a7acd712229cca20e12cc1b860a
Mariano <mscalella911@gmail.com>
2015-12-28 18:11:58 -0300

    'sdfsadf'
    Former-commit-id: b64a07b21bf332c85ac29a7b54a73a8342285d3b

--- Data/FuncionesGenerales.cs
+++ Data/FuncionesGenerales.cs
@@ -27,7 +27,7 @@ namespace ProntoMVC.Data
             StringBuilder sb = new StringBuilder();
             foreach (char c in str)
             {
-                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_' || c == ' ' || c == '-' || c == '&')
                 {
                     sb.Append(c);
                 }
