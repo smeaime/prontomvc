45175b281bd1ffd74c020655623345bb6fcd7770
Mariano <mscalella911@gmail.com>
2016-01-29 14:30:35 -0300

    'sdfsadf'
    Former-commit-id: 330deaa43f85afcaaf62ae881032d887064bc881 [formerly a39c8e97534213bd34f8c189e071c2a5d49dcac1]
    Former-commit-id: 625f640df51fb89abcc49b671fa4032777a8e438

---
+++ ProntoWindowsService/instalar prontoagente local.bat
@@ -0,0 +1,13 @@
+C:\Users\Administrador\Documents\bdl\pronto\ProntoWindowsService\bin\Debug
+
+net stop prontoagente 
+pause 
+
+prontoagente --uninstall
+pause 
+
+prontoagente --install
+pause
+
+net start prontoagente 
+pause
