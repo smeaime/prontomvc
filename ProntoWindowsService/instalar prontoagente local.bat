42cdeb5d65629ab528b3e6eb11206863346dd7c6
Mariano <mscalella911@gmail.com>
2016-01-29 14:47:12 -0300

    'sdfsadf'
    Former-commit-id: c03e0675ad842512a2bea6f94c36a737eb8bf14f [formerly bb6e0ac66febc715a999c4c4cdc75946f704c21c]
    Former-commit-id: 3f3ca9220f12aa3df9c8336c898bc10f12302619

--- ProntoWindowsService/instalar prontoagente local.bat
+++ ProntoWindowsService/instalar prontoagente local.bat
@@ -1,4 +1,4 @@
-C:\Users\Administrador\Documents\bdl\pronto\ProntoWindowsService\bin\Debug
+cd C:\Users\Administrador\Documents\bdl\pronto\ProntoWindowsService\bin\Debug
 
 net stop prontoagente 
 pause 
