8f2fba3bf7134ee69116b5f00aa1c5ceb9f23ea2
Mariano <mscalella911@gmail.com>
2016-02-15 23:50:44 -0300

    'sdfsadf'
    Former-commit-id: 5ac08c672c2ae1f5e6a3d516c7af759c02c08ecb [formerly 6a8374d6501cb00049b173f4a7a722a760e340a9]
    Former-commit-id: eec09aebeef120ec32271c500926218e0d65feb3

--- TestProject2/TestWilliams.cs
+++ TestProject2/TestWilliams.cs
@@ -210,7 +210,7 @@ namespace ProntoMVC.Tests
             zipFile = @"C:\Users\Administrador\Documents\bdl\prontoweb\Documentos\imagenes\Nueva carpeta.zip";
             zipFile = @"C:\Users\Administrador\Documents\bdl\prontoweb\Documentos\imagenes\doc02102016173229.zip";
             zipFile = @"C:\Users\Administrador\Documents\bdl\prontoweb\Documentos\imagenes\tandabuena.zip";
-
+            zipFile = @"C:\Users\Administrador\Documents\bdl\prontoweb\Documentos\imagenes\doc02152016123436.zip";
 
             /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
