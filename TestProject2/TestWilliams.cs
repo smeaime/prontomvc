f5c276490f3459810eb5e9f5de92bf478e4549b7
Mariano <mscalella911@gmail.com>
2016-08-31 19:14:47 -0300

    'sdfsadf'
    Former-commit-id: 8280b534cf52405896b7cfaa2ca6ebb2768f4bde

--- TestProject2/TestWilliams.cs
+++ TestProject2/TestWilliams.cs
@@ -239,6 +239,120 @@ namespace ProntoMVC.Tests
 
 
 
+        [TestMethod]
+        public void liquidacionsubcon_22294()
+        {
+
+
+            // Sería agregar dos tarifas más que servirían para la liquidación de subcontratistas, especialmente para Vagones, separado por  Calada y Descarga
+
+
+            var c = CartaDePorteManager.GetItem(SC, 1372987);
+            c.SubnumeroVagon = 222;
+            string ms = "";
+            CartaDePorteManager.Save(SC, c, 1, "", false, ref ms);
+
+            ParametroManager.GuardarValorParametro2(SC, "DestinosDeCartaPorteApartadosEnLiquidacionSubcontr", "NOBLE ARG. - Lima|CHIVILCOY|CARGILL - San Justo|FABRICA VICENTIN|PUERTO VICENTIN");
+
+
+            ReportViewer ReporteLocal = new Microsoft.Reporting.WebForms.ReportViewer();
+
+            ReportParameter p2 = null;
+            string sTitulo = "";
+
+            var q = ConsultasLinq.LiquidacionSubcontratistas(SC,
+                       "", "", "", 1, 2000,
+                        CartaDePorteManager.enumCDPestado.Todas, "", -1, -1,
+                       -1, -1,
+                       -1, -1, -1, -1, CartaDePorteManager.FiltroANDOR.FiltroOR, "Ambos",
+                        new DateTime(2014, 1, 1),
+                        new DateTime(2014, 1, 10),
+                        -1, -1, ref sTitulo);
+
+
+
+            string titulo = EntidadManager.NombreCliente(SC, 105);
+
+
+            ReportParameter[] p = new ReportParameter[5];
+            p[0] = new ReportParameter("Concepto1", "");
+            p[1] = new ReportParameter("titulo", "");
+            p[2] = new ReportParameter("Concepto2", "");
+            p[3] = new ReportParameter("Concepto1Importe", "-1");
+            p[4] = new ReportParameter("Concepto2Importe", "-1");
+
+
+            string output = "";
+
+            CartaDePorteManager.RebindReportViewerLINQ_Excel
+                                (ref ReporteLocal, @"C:\Users\Administrador\Documents\bdl\pronto\prontoweb\ProntoWeb\Informes\Liquidación de SubContratistas 2.rdl", q, ref output, p);
+
+            System.Diagnostics.Process.Start(output);
+
+        }
+
+
+
+
+
+
+
+        [TestMethod]
+        public void PegatinaRamalloBunge_22408()
+        {
+
+            //explota
+
+            string ms = "";
+
+            //string archivoExcel = @"C:\Users\Administrador\Desktop\Anali19.txt";  // tabs
+            string archivoExcel = @"C:\Users\Administrador\Documents\bdl\pronto\prontoweb\Documentos\pegatinas\bungeramallo.txt";
+            //archivoExcel = @"C:\Users\Administrador\Desktop\Anali19.d";   // punto y coma
+
+            int m_IdMaestro = 0;
+            Pronto.ERP.BO.CartaDePorte carta;
+
+
+            // escribir descarga de una carta
+            //carta = null;
+            //carta = CartaDePorteManager.GetItemPorNumero(SC, 549768066, 0, 0);
+            //carta.NobleGrado = 2;
+            //CartaDePorteManager.Save(SC, carta, 1, "lalala", true, ref ms);
+            //Assert.AreEqual(30000, carta.NetoFinalIncluyendoMermas);
+
+
+
+
+            string log = "";
+            //hay que pasar el formato como parametro 
+            ExcelImportadorManager.FormatearExcelImportadoEnDLL(ref m_IdMaestro, archivoExcel,
+                                    LogicaImportador.FormatosDeExcel.BungeRamalloDescargaTexto, SC, 0,
+                                    ref log, new DateTime(2016, 5, 1).ToShortDateString(), 0, "");
+
+            var dt = LogicaImportador.TraerExcelDeBase(SC, ref  m_IdMaestro);
+
+            foreach (System.Data.DataRow r in dt.Rows)
+            {
+                var dr = r;
+                var c = LogicaImportador.GrabaRenglonEnTablaCDP(ref dr, SC, null, null, null,
+                                                        null, null, null, null,
+                                                        null, null);
+            }
+
+
+
+
+            //verificar que sigue así
+            carta = null;
+            carta = CartaDePorteManager.GetItemPorNumero(SC, 549768066, 0, 0);
+            carta.NobleGrado = 2;
+            CartaDePorteManager.Save(SC, carta, 1, "lalala", true, ref ms);
+            Assert.AreEqual(30000, carta.NetoFinalIncluyendoMermas);
+
+           
+        }
+
+
 
 
 
@@ -246,9 +360,21 @@ namespace ProntoMVC.Tests
         public void acopio_23480_22255()
         {
 
-            LogicaFacturacion.LeyendaAcopio(86082, SC);
+           //caso 1: si es LDC y elevacion (o sea exportacion), NO mostrar "ACOPIO OTROS" (pero sí los demás)
+           var s1 = LogicaFacturacion.LeyendaAcopio(85759, SC);
+
+           // caso 2: había acopios de distintos clientes (ACA PEHUAJO en factura de LDC). Usar solamente los del cliente facturado
+           var s2 = LogicaFacturacion.LeyendaAcopio(85760, SC);
+
+            
+
+           var s3= LogicaFacturacion.LeyendaAcopio(86082, SC);
+           //Assert.AreEqual(s3, "");
+
 
         }
+
+
 
 
 
@@ -754,118 +880,6 @@ namespace ProntoMVC.Tests
 
             CartaDePorteManager.ReasignoTarifaSubcontratistasDeTodasLasCDPsDescargadasSinFacturarYLasGrabo(SC, 1, "nano", 2066);
 
-        }
-
-
-        [TestMethod]
-        public void liquidacionsubcon_22294()
-        {
-
-
-            // Sería agregar dos tarifas más que servirían para la liquidación de subcontratistas, especialmente para Vagones, separado por  Calada y Descarga
-
-
-            var c = CartaDePorteManager.GetItem(SC, 1372987);
-            c.SubnumeroVagon = 222;
-            string ms = "";
-            CartaDePorteManager.Save(SC, c, 1, "", false, ref ms);
-
-            ParametroManager.GuardarValorParametro2(SC, "DestinosDeCartaPorteApartadosEnLiquidacionSubcontr", "NOBLE ARG. - Lima|CHIVILCOY|CARGILL - San Justo|FABRICA VICENTIN|PUERTO VICENTIN");
-
-
-            ReportViewer ReporteLocal = new Microsoft.Reporting.WebForms.ReportViewer();
-
-            ReportParameter p2 = null;
-            string sTitulo = "";
-
-            var q = ConsultasLinq.LiquidacionSubcontratistas(SC,
-                       "", "", "", 1, 2000,
-                        CartaDePorteManager.enumCDPestado.Todas, "", -1, -1,
-                       -1, -1,
-                       -1, -1, -1, -1, CartaDePorteManager.FiltroANDOR.FiltroOR, "Ambos",
-                        new DateTime(2014, 1, 1),
-                        new DateTime(2014, 1, 10),
-                        -1, -1, ref sTitulo);
-
-
-
-            string titulo = EntidadManager.NombreCliente(SC, 105);
-
-
-            ReportParameter[] p = new ReportParameter[5];
-            p[0] = new ReportParameter("Concepto1", "");
-            p[1] = new ReportParameter("titulo", "");
-            p[2] = new ReportParameter("Concepto2", "");
-            p[3] = new ReportParameter("Concepto1Importe", "-1");
-            p[4] = new ReportParameter("Concepto2Importe", "-1");
-
-
-            string output = "";
-
-            CartaDePorteManager.RebindReportViewerLINQ_Excel
-                                (ref ReporteLocal, @"C:\Users\Administrador\Documents\bdl\pronto\prontoweb\ProntoWeb\Informes\Liquidación de SubContratistas 2.rdl", q, ref output, p);
-
-            System.Diagnostics.Process.Start(output);
-
-        }
-
-
-
-
-
-
-
-        [TestMethod]
-        public void PegatinaRamalloBunge_22408()
-        {
-
-            //explota
-
-            string ms = "";
-
-            //string archivoExcel = @"C:\Users\Administrador\Desktop\Anali19.txt";  // tabs
-            string archivoExcel = @"C:\Users\Administrador\Documents\bdl\pronto\prontoweb\Documentos\pegatinas\bungeramallo.txt";
-            //archivoExcel = @"C:\Users\Administrador\Desktop\Anali19.d";   // punto y coma
-
-            int m_IdMaestro = 0;
-            Pronto.ERP.BO.CartaDePorte carta;
-
-
-            // escribir descarga de una carta
-            //carta = null;
-            //carta = CartaDePorteManager.GetItemPorNumero(SC, 549768066, 0, 0);
-            //carta.NobleGrado = 2;
-            //CartaDePorteManager.Save(SC, carta, 1, "lalala", true, ref ms);
-            //Assert.AreEqual(30000, carta.NetoFinalIncluyendoMermas);
-
-
-
-
-            string log = "";
-            //hay que pasar el formato como parametro 
-            ExcelImportadorManager.FormatearExcelImportadoEnDLL(ref m_IdMaestro, archivoExcel,
-                                    LogicaImportador.FormatosDeExcel.BungeRamalloDescargaTexto, SC, 0,
-                                    ref log, new DateTime(2016, 5, 1).ToShortDateString(), 0, "");
-
-            var dt = LogicaImportador.TraerExcelDeBase(SC, ref  m_IdMaestro);
-
-            foreach (System.Data.DataRow r in dt.Rows)
-            {
-                var dr = r;
-                var c = LogicaImportador.GrabaRenglonEnTablaCDP(ref dr, SC, null, null, null,
-                                                        null, null, null, null,
-                                                        null, null);
-            }
-
-
-
-
-            //verificar que sigue así
-            carta = null;
-            carta = CartaDePorteManager.GetItemPorNumero(SC, 549768066, 0, 0);
-            carta.NobleGrado = 2;
-            CartaDePorteManager.Save(SC, carta, 1, "lalala", true, ref ms);
-            Assert.AreEqual(30000, carta.NetoFinalIncluyendoMermas);
         }
 
 
