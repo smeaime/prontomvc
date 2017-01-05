b9368af9dc236634bee239dfa990220358cf2ed6
Mariano <mscalella911@gmail.com>
2017-01-05 17:42:07 -0300

    'sdfsadf'
    Former-commit-id: 1bcf8769e8557fa5023f35f2ad6f9f0271ac7473

--- prontoweb/Novedades/otros/SQLQuery1.sql
+++ prontoweb/Novedades/otros/SQLQuery1.sql
@@ -1,10 +1,9 @@
-use wDemoWilliams
+use AdministradorProyecto
 
-
-
- exec  
-
-
-
-
-
+select R.IdReclamo, R.TituloReclamo + CAST( C.Comentario as varchar(2000)) ,  R.TituloReclamo,C.Comentario,c.FechaComentario  
+from comentariosreclamo  C --para ver qué hiciste, revisá tus visitas a stackoverflow
+left join Reclamos R on C.idreclamo=R.IdReclamo
+where idusuario=125 and 
+        FechaComentario between '11/28/2016' and '5/1/2017'
+and (comentario like '%Test%' ) -- or not comentario like '%Reclamo%' )
+order by idcomentario asc 
