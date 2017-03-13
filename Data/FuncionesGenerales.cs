f5de8e32f5e77bde8c2c3e5cfb3e5698e6720bcb
Mariano <mscalella911@gmail.com>
2017-03-13 12:59:22 -0300

    'sdfsadf'
    Former-commit-id: 1a37f9e70af4516bf38ec4b490b252c019054b99

--- Data/FuncionesGenerales.cs
+++ Data/FuncionesGenerales.cs
@@ -21,8 +21,9 @@ using ProntoMVC.Data.Models;
 using System;
 
 
+using System.Data;
 
-
+using OfficeOpenXml; //EPPLUS, no confundir con el OOXML
 
 
 
@@ -32,6 +33,42 @@ namespace ProntoMVC.Data
 {
     public class FuncionesGenericasCSharp
     {
+
+
+
+                // http://stackoverflow.com/questions/13396604/excel-to-datatable-using-epplus-excel-locked-for-editing
+        public static DataTable GetDataTableFromExcel(string path, bool hasHeader = true)
+        {
+            using (var pck = new OfficeOpenXml.ExcelPackage())
+            {
+                using (var stream = File.OpenRead(path))
+                {
+                    pck.Load(stream);
+                }
+                var ws = pck.Workbook.Worksheets.First();
+                DataTable tbl = new DataTable();
+                foreach (var firstRowCell in ws.Cells[1, 1, 1, ws.Dimension.End.Column])
+                {
+                    tbl.Columns.Add(hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column));
+                }
+                var startRow = hasHeader ? 2 : 1;
+                for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
+                {
+                    var wsRow = ws.Cells[rowNum, 1, rowNum, ws.Dimension.End.Column];
+                    DataRow row = tbl.Rows.Add();
+                    foreach (var cell in wsRow)
+                    {
+                        row[cell.Start.Column - 1] = cell.Text;
+                    }
+                }
+                return tbl;
+            }
+        }
+    
+
+
+
+
 
         // funciones C# usadas por businesslogic (hecha en VB.net). Migrar del .Data a una dll aparte
 
