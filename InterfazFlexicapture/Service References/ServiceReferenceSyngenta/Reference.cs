939bc69a33592ce68e51131942d7905ce4f1c0ed
Mariano <mscalella911@gmail.com>
2017-07-28 11:00:01 -0300

    'sdfsadf'
    Former-commit-id: c1d5da4114d865b02d6e54cb7face3d0998f8743 [formerly e898ac95b5ee96e097ed9779480eff28eb5abeae]
    Former-commit-id: 278bd1f3588d16930cf326e1edef72b087c2d746

--- InterfazFlexicapture/Service References/ServiceReferenceSyngenta/Reference.cs
+++ InterfazFlexicapture/Service References/ServiceReferenceSyngenta/Reference.cs
@@ -22,7 +22,7 @@ namespace InterfazFlexicapture.ServiceReferenceSyngenta {
     }
     
     /// <remarks/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.6.1099.0")]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.7.2046.0")]
     [System.SerializableAttribute()]
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.ComponentModel.DesignerCategoryAttribute("code")]
@@ -348,7 +348,7 @@ namespace InterfazFlexicapture.ServiceReferenceSyngenta {
     }
     
     /// <remarks/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.6.1099.0")]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.7.2046.0")]
     [System.SerializableAttribute()]
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.ComponentModel.DesignerCategoryAttribute("code")]
