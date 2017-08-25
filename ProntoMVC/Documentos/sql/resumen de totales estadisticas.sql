9e6b874fa61e7aa1b7cef9a6b042b80090a769db
Mariano <mscalella911@gmail.com>
2017-08-25 04:06:37 -0300

    'sdfsadf'
    Former-commit-id: 91f330663afe666d8a908c9811e0ef17f779b0af

--- ProntoMVC/Documentos/sql/resumen de totales estadisticas.sql
+++ ProntoMVC/Documentos/sql/resumen de totales estadisticas.sql
@@ -235,6 +235,201 @@ go
 
 
 
+IF EXISTS ( SELECT  *
+            FROM    dbo.sysobjects
+            WHERE   id = OBJECT_ID(N'[dbo].[wCartasDePorte_TX_DetalleDeTotalesEstadisticas]')
+                    AND OBJECTPROPERTY(id, N'IsProcedure') = 1 ) 
+    DROP PROCEDURE [dbo].[wCartasDePorte_TX_DetalleDeTotalesEstadisticas]
+go
+
+
+
+CREATE PROCEDURE [dbo].[wCartasDePorte_TX_DetalleDeTotalesEstadisticas]
+
+			 @startRowIndex int  = NULL,
+            @maximumRows int  = NULL,
+            @estado int  = NULL,
+            @QueContenga  VARCHAR(50) = NULL,
+            @idVendedor int  = NULL,
+            @idCorredor int  = NULL,
+            @idDestinatario int  = NULL,
+            @idIntermediario int  = NULL,
+            @idRemComercial int  = NULL,
+            @idArticulo int  = NULL,
+            @idProcedencia int  = NULL,
+            @idDestino int  = NULL,
+            @AplicarANDuORalFiltro int  = NULL,
+            @ModoExportacion  VARCHAR(20) = NULL,
+
+			@fechadesde datetime,
+			@fechahasta datetime,
+
+            @puntoventa int  = NULL, 
+            @optDivisionSyngenta  VARCHAR(50) = NULL,
+           --@bTraerDuplicados As Boolean = False,
+            @Contrato  VARCHAR(50) = NULL, 
+            @QueContenga2  VARCHAR(50) = NULL,
+            @idClienteAuxiliarint int  = NULL,
+            @AgrupadorDeTandaPeriodos int  = NULL,
+            @Vagon  int  = NULL,
+			@Patente VARCHAR(10) = NULL,
+            @optCamionVagon  VARCHAR(10) = NULL,
+
+
+            @fechadesdeAnterior As DateTime= NULL,
+			@fechahastaAnterior As DateTime= NULL
+
+AS
+
+
+
+
+select *
+from 
+(
+select
+
+month(FechaDescarga) as  mes  , 
+year(FechaDescarga) as año, 
+            cdp.puntoventa as Sucursal,
+			Exporta as Modo,
+			Count(*) as CantCartas,
+			sum(
+				case when FechaDescarga between @FechaDesde and @FechaHasta
+				then NetoFinal/1000
+				else 0 	end
+			   ) as NetoFinal,
+			sum(
+				case when FechaDescarga between @FechaDesdeAnterior and @FechaHastaAnterior  
+				then NetoFinal/1000
+				else 0 	end
+			   ) as PeriodoAnterior  ,
+			   
+			
+			  sum(
+				case when FechaDescarga between @FechaDesdeAnterior and @FechaHastaAnterior  
+				then DETFAC.PrecioUnitario * NetoFinal / 1000
+				else 0 	end
+			   ) 
+			    as Facturado
+			
+
+
+from 
+ dbo.fSQL_GetDataTableFiltradoYPaginado  
+				(  
+								
+
+							@startRowIndex, 
+							@maximumRows, 
+							@estado,
+							@QueContenga, 
+							@idVendedor, 
+
+							@idCorredor, 
+							@idDestinatario, 
+							@idIntermediario,
+							@idRemComercial, 
+							@idArticulo,
+
+							@idProcedencia,
+							@idDestino,
+							@AplicarANDuORalFiltro,
+							@ModoExportacion,
+				
+							@FechaDesdeAnterior,
+
+							@FechaHasta,
+				
+							@puntoventa, 
+							NULL,
+							 'FALSE',--duplicados?
+							@Contrato, 
+
+							@QueContenga2, 
+							@idClienteAuxiliarint, 
+							@AgrupadorDeTandaPeriodos, 
+							@Vagon,
+							@Patente, 
+
+							@optCamionVagon
+					)
+    
+ as CDP
+
+left join detallefacturas DETFAC on CDP.IdDetalleFactura=DETFAC.IdDetalleFactura
+
+ Group by  cdp.PuntoVenta,cdp.exporta
+
+,month(FechaDescarga)  , 
+year(FechaDescarga) 
+
+
+
+union 
+
+select 
+
+
+month(FechaIngreso) as  mes  , 
+year(FechaIngreso) as año, 
+                   IdStock as Sucursal,
+			'Buque' as Modo,
+			Count(*) as CantCartas,
+			sum(
+				case when i.FechaIngreso >= @FechaDesde
+				then i.cantidad /1000
+				else 0 	end
+			   ) as NetoFinal,
+			sum(
+				case when i.FechaIngreso <= @FechaHastaAnterior 
+				then i.cantidad /1000
+				else 0 	end
+			   ) as PeriodoAnterior           ,
+
+			   0 as Facturado
+			         
+			
+
+From CartasPorteMovimientos i
+
+left join detallefacturas DETFAC on i.IdDetalleFactura=DETFAC.IdDetalleFactura
+
+
+Where (
+                                i.Tipo = 4 
+                                And isnull(i.Anulada, 'NO') <> 'SI' 
+                                And i.FechaIngreso >= @FechaDesdeAnterior And i.FechaIngreso <= @FechaHasta 
+                           )
+
+
+
+ 
+
+ Group by  IdStock
+ 
+,month(FechaIngreso) , 
+year(FechaIngreso)
+
+) as Q
+
+order by año, mes
+
+
+
+
+go
+
+
+
+
+
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 --//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 --//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -258,6 +453,52 @@ set @FechaDesdeAnterior ='20151101'
 set @FechaHastaAnterior='20160531'
 set @FechaDesde='20161101'
 set @FechaHasta='20170531'
+
+
+
+
+
+
+
+exec [wCartasDePorte_TX_DetalleDeTotalesEstadisticas] 
+					NULL, 
+					NULL, 
+					4,
+					NULL, 
+					NULL, 
+
+					NULL, 
+					NULL, 
+					NULL,
+					NULL, 
+					-1, --@idArticulo,
+
+					NULL, --@idProcedencia,
+					-1,---1, --@idDestino,
+					NULL, --@AplicarANDuORalFiltro,
+					'Export', --'Entregas', --'Ambos', --'Buques',
+						
+					
+@FechaDesde, @FechaHasta,
+
+
+					NULL, 
+					NULL,
+					NULL, 
+					NULL, 
+
+					NULL, 
+					NULL, 
+					NULL,
+					NULL, 
+					NULL
+
+,@FechaDesdeAnterior , @FechaHastaAnterior
+
+
+
+
+
 
 
 
@@ -309,6 +550,10 @@ exec [wCartasDePorte_TX_ResumenDeTotalesEstadisticas]
 
 
 
+
+
+/*
+
 exec [wCartasDePorte_TX_EstadisticasDeDescarga] 
 					NULL, 
 					NULL, 
@@ -347,7 +592,7 @@ go
 
 
 
-
+*/
 
 
 
