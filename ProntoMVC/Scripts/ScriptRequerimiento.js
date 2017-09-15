af5f759c8666eecf49cc09564d8688ee4c13ba44
Mariano <mscalella911@gmail.com>
2017-09-15 01:35:26 -0300

    'sdfsadf'
    Former-commit-id: 9e82a2e8bb18d4576f4545812480b797c1678808

--- ProntoMVC/Scripts/ScriptRequerimiento.js
+++ ProntoMVC/Scripts/ScriptRequerimiento.js
@@ -869,6 +869,8 @@ $(function () {
                     },
 
 
+
+
                      { name: 'IdEquipoDestino', index: 'IdEquipoDestino', label: 'TB', hidden: true },
                      {
                          name: 'EquipoDestino', formoptions: { rowpos: 22, colpos: 1, label: "EquipoDestino" },
@@ -878,7 +880,7 @@ $(function () {
                              dataInit: function (elem) {
                                  var NoResultsLabel = "No se encontraron resultados";
                                  $(elem).autocomplete({
-                                     source: ROOT + "Articulo/GetArticulosAutocomplete2", minLength: 0,
+                                     source: ROOT + "Articulo/GetCodigosArticulosAutocomplete_Equipos", minLength: 0,
                                      select: function (event, ui) {
                                          if (ui.item.value === NoResultsLabel) {
                                              event.preventDefault();
@@ -886,12 +888,12 @@ $(function () {
                                          }
                                          $("#IdArticulo").val(ui.item.id);
                                          $("#Codigo").val(ui.item.codigo);
-                                         $("#IdUnidad").val(ui.item.IdUnidad);
+                                         //$("#IdUnidad").val(ui.item.IdUnidad);
                                          //$("#Unidad").val(ui.item.Unidad);
-                                         $("#Unidad").val(ui.item.IdUnidad); // hay que ponerle el id para elegir el item... por eso es que cuando salis del form en la celda queda con el id como texto...  no?
+                                         //$("#Unidad").val(ui.item.IdUnidad); // hay que ponerle el id para elegir el item... por eso es que cuando salis del form en la celda queda con el id como texto...  no?
 
                                          UltimoIdArticulo = ui.item.id;
-                                         UltimoIdUnidad = ui.item.IdUnidad;
+                                         //UltimoIdUnidad = ui.item.IdUnidad;
                                      },
                                      focus: function (event, ui) {
                                          if (ui.item.value === NoResultsLabel) {
@@ -926,12 +928,12 @@ $(function () {
                                                  }
                                                  $("#IdArticulo").val(ui.id);
                                                  $("#Codigo").val(ui.codigo);
-                                                 $("#IdUnidad").val(ui.IdUnidad);
+                                                 //$("#IdUnidad").val(ui.IdUnidad);
                                                  //$("#Unidad").val(ui.item.Unidad);
-                                                 $("#Unidad").val(ui.IdUnidad); // hay que ponerle el id para elegir el item... por eso es que cuando salis del form en la celda queda con el id como texto...  no?
+                                                 //$("#Unidad").val(ui.IdUnidad); // hay que ponerle el id para elegir el item... por eso es que cuando salis del form en la celda queda con el id como texto...  no?
 
                                                  UltimoIdArticulo = ui.id;
-                                                 UltimoIdUnidad = ui.IdUnidad;
+                                                 //UltimoIdUnidad = ui.IdUnidad;
                                              }
                                              else {
                                                  alert("No existe el artículo"); // se está bancando que no sea identica la descripcion
