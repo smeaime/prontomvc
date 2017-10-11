dc2320c249b69174bfb400666f81a9e12f7159bf
Mariano <mscalella911@gmail.com>
2017-10-11 19:52:35 -0300

    'sdfsadf'
    Former-commit-id: ed6855f855358eecddcf5eae8d6245d59887ae6b [formerly e5033b14180cb4f1e8ae735b2ba697a208896b12]
    Former-commit-id: 4a5f257595905f2672e8a902e6f41b18688f7b16

--- prontoweb/ProntoWeb/scripts/constants.js
+++ prontoweb/ProntoWeb/scripts/constants.js
@@ -2,10 +2,14 @@
 
 window.gauntface = window.gauntface || {};
 window.gauntface.CONSTANTS = {
-  GCM_API_KEY: 'AIzaSyBBh4ddPa96rQQNxqiq_qQj7sq1JdsNQUQ',
+    GCM_API_KEY: 'AIzaSyBuC5oWSsrDcmVBa3EIG_-j5le-81mH9E4',
+
+
   APPLICATION_KEYS: {
     publicKey: 'BDd3_hVL9fZi9Ybo2UUzA284WG5FZR30_95YeZJsiA' +
       'pwXKpNcF1rRPF3foIiBHXRdJI2Qhumhf6_LFTeZaNndIo',
     privateKey: 'xKZKYRNdFFn8iQIF2MH54KTfUHwH105zBdzMR7SI3xI',
-  },
+    },
+
+
 };
