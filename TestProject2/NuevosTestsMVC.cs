e394716348f8f950ac6dcb5c8946966ee56cce0f
Mariano <mscalella911@gmail.com>
2017-11-08 15:15:16 -0300

    'sdfsadf'
    Former-commit-id: 59d9ad236ad97a0366419beb4f96c571dcc7993d

--- TestProject2/NuevosTestsMVC.cs
+++ TestProject2/NuevosTestsMVC.cs
@@ -159,13 +159,11 @@ namespace ProntoMVC.TestsMVC
         [TestMethod]
         public void envio_rms_por_mail()
         {
-            // como puedo crear una RM para que aparezca en el "RMs Pendientes de Asignar"?
-
             var c = new RequerimientoController();
             GetMockedControllerGenerico(c);
 
 
-            c.EnviarEmail(10, 10, "SI");
+            //c.EnviarEmail(10, 10, "SI");
         }
 
 
