2078b42c37aef3f60481b461635a588b872e4bb5
Mariano <mscalella911@gmail.com>
2017-11-21 15:35:01 -0300

    'sdfsadf'
    Former-commit-id: 990350667fc9b1ccfd51ad22fa7868e669b9d3bd [formerly ebcbf35751445fa598bcf4ee53b7c48a672db3f5]
    Former-commit-id: d62e62bc553d2f8dd18c11c08b7e9f8f6b8e9764

--- prontoweb/Documentos/SQLQuery1.sql
+++ prontoweb/Documentos/SQLQuery1.sql
@@ -3,21 +3,43 @@
 select top 2 * from Facturas
 order by idfactura desc
 
-select top 5000  detallefacturas.*,facturas.observaciones from DetalleFacturas
+
+
+select
+--top 5000  
+--detallefacturas.*,
+facturas.observaciones,
+facturas.puntoventa, facturas.numerofactura
+from DetalleFacturas
 left join facturas on facturas.idfactura=detallefacturas.idfactura
-where porcentajeiva is not null
-order by idfactura desc
+where porcentajeiva is not null and (facturas.observaciones like '%periodo%' or facturas.observaciones like '%corredor%')
+order by detallefacturas.idfactura desc
 
 
-select top 1000  * from DetalleFacturas
-where porcentajeiva is not null
+
+select top 1000 DetalleFacturas.*,facturas.importeiva1
+from DetalleFacturas
+left join facturas on facturas.idfactura=detallefacturas.idfactura
+--where porcentajeiva is not null
 order by idfactura desc
 
 
 select * from DetalleFacturas
 where IdFactura = 100733
 
-select * from Facturas
+select observaciones,* from Facturas
 where IdFactura = 100733
+
+
+select detallefacturas.*,facturas.observaciones,clientes.razonsocial,facturas.puntoventa, facturas.*
+from DetalleFacturas
+left join facturas on facturas.idfactura=detallefacturas.idfactura
+left join clientes on facturas.idcliente=clientes.idcliente
+where 
+--numerofactura = 13570 and puntoventa=20
+(facturas.numerofactura = 6515 and facturas.puntoventa=20) OR
+(facturas.numerofactura = 13570 and facturas.puntoventa=20) OR
+(facturas.numerofactura = 14396 and facturas.puntoventa=20) 
+
 
 
