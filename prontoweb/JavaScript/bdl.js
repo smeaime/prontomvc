ae19230bdfc4225a195a596ac6ceec212867e312
Mariano <mscalella911@gmail.com>
2017-11-23 21:47:49 -0300

    'sdfsadf'
    Former-commit-id: 92f858ef588931b1cef5cc7ffa1a223276d8ffda [formerly b2cf3645bd21483dbff6b9d8748ccf67901f6dea]
    Former-commit-id: c6ebb08728c2a7590c919e0986cb877c631e3ad3

--- prontoweb/JavaScript/bdl.js
+++ prontoweb/JavaScript/bdl.js
@@ -1,4 +1,21 @@
-﻿var ie5 = (document.getElementById && document.all);
+﻿
+var qs = (function (a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i) {
+        var p = a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
+
+
+
+var ie5 = (document.getElementById && document.all);
 var ns6 = (document.getElementById && !document.all);
 var custWindowOpen = false;
 var ListWindowOpen = false;
