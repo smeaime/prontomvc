a61d935285c4f0038647d83d3133c12b1b664c2f
DESKTOP-TVDQV7C\Mariano <mscalella911@gmail.com>
2018-02-20 11:55:07 -0300

    'sdfsadf'
    Former-commit-id: 4286b3d27ae68d55d6bc61711182d1ad841c1451

--- prontoweb/ProntoWeb/ReporteResumenEstad.aspx.vb
+++ prontoweb/ProntoWeb/ReporteResumenEstad.aspx.vb
@@ -676,40 +676,7 @@ Namespace ProntoMVC.Reportes
 
 
 
-    Public NotInheritable Class CustomReportCredentials
-        Implements IReportServerCredentials
-        Private _UserName As String
-        Private _PassWord As String
-        Private _DomainName As String
 
-        Public Sub New(UserName As String, PassWord As String, DomainName As String)
-            _UserName = UserName
-            _PassWord = PassWord
-            _DomainName = DomainName
-        End Sub
-
-        Public ReadOnly Property ImpersonationUser() As System.Security.Principal.WindowsIdentity Implements IReportServerCredentials.ImpersonationUser
-            Get
-                Return Nothing
-            End Get
-        End Property
-
-        Public ReadOnly Property NetworkCredentials() As ICredentials Implements IReportServerCredentials.NetworkCredentials
-            Get
-                Return New NetworkCredential(_UserName, _PassWord, _DomainName)
-            End Get
-        End Property
-
-        Public Function GetFormsCredentials(ByRef authCookie As Cookie, ByRef user As String, ByRef password As String, ByRef authority As String) As Boolean Implements IReportServerCredentials.GetFormsCredentials
-            authCookie = Nothing
-            user = InlineAssignHelper(password, InlineAssignHelper(authority, Nothing))
-            Return False
-        End Function
-        Private Shared Function InlineAssignHelper(Of T)(ByRef target As T, value As T) As T
-            target = value
-            Return value
-        End Function
-    End Class
 End Namespace
 
 
